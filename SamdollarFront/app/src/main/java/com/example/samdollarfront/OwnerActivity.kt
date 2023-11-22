4b226766e5cd89e0839da96bf651a730f9237ad1
jjeongee <starstar117@naver.com>
2023-11-22 18:17:10 +0900

    1121 frontEnd

---
+++ SamdollarFront/app/src/main/java/com/example/samdollarfront/OwnerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.samdollarfront
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OwnerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_owner)
+    }
+}
---
+++ SamdollarFront/app/src/main/java/com/example/samdollarfront/StoreActivity.kt
@@ -0,0 +1,11 @@
+package com.example.samdollarfront
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_store)
+    }
+}
